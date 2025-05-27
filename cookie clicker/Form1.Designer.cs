e59ac4ec9ab46310e1b257cd280282ce11c7194e
alex daraz <alexdaraz750@gmail.com>
2025-05-27 19:07:10 +0100

    Add project files.

---
+++ cookie clicker/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace cookie_clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cookie = new System.Windows.Forms.Button();
+            this.lblmoney = new System.Windows.Forms.LinkLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel6 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Cookie
+            // 
+            this.Cookie.Location = new System.Drawing.Point(322, 184);
+            this.Cookie.Name = "Cookie";
+            this.Cookie.Size = new System.Drawing.Size(148, 40);
+            this.Cookie.TabIndex = 0;
+            this.Cookie.Text = "cookie";
+            this.Cookie.UseVisualStyleBackColor = true;
+            this.Cookie.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblmoney
+            // 
+            this.lblmoney.ActiveLinkColor = System.Drawing.Color.RoyalBlue;
+            this.lblmoney.AutoSize = true;
+            this.lblmoney.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblmoney.Location = new System.Drawing.Point(366, 83);
+            this.lblmoney.Name = "lblmoney";
+            this.lblmoney.Size = new System.Drawing.Size(55, 13);
+            this.lblmoney.TabIndex = 1;
+            this.lblmoney.TabStop = true;
+            this.lblmoney.Text = "linkLabel1";
+            this.lblmoney.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "auto clicker";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 55);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Multiplyer Upgrade";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 55);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Clicking Speed";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(120, 143);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(36, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "price1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(120, 204);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(36, 13);
+            this.linkLabel2.TabIndex = 13;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "price2";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(120, 265);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(36, 13);
+            this.linkLabel3.TabIndex = 14;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "price3";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Location = new System.Drawing.Point(682, 143);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel4.TabIndex = 15;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "linkLabel4";
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Location = new System.Drawing.Point(682, 204);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel5.TabIndex = 16;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "linkLabel5";
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // linkLabel6
+            // 
+            this.linkLabel6.AutoSize = true;
+            this.linkLabel6.Location = new System.Drawing.Point(682, 265);
+            this.linkLabel6.Name = "linkLabel6";
+            this.linkLabel6.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel6.TabIndex = 17;
+            this.linkLabel6.TabStop = true;
+            this.linkLabel6.Text = "linkLabel6";
+            this.linkLabel6.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel6_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel6);
+            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.linkLabel4);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblmoney);
+            this.Controls.Add(this.Cookie);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Cookie;
+        private System.Windows.Forms.LinkLabel lblmoney;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.LinkLabel linkLabel6;
+    }
+}
+
---
+++ cookie clicker/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cookie_clicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double money = 1, multiplyer = 1,clickspeed=1000,amountofclickers=0,price1 = 10,price2 = 10,price3 = 10;
+        bool isRunning = true;
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Text = price3.ToString();
+            linkLabel2.Text = price1.ToString();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (money >= price3)
+            {
+                money = money - price3;
+                amountofclickers++;
+                linkLabel3.Text = "Purchase complete";
+                await Task.Delay(500);
+                price3 = price3 * 3;
+                Text = price3.ToString();
+                linkLabel3.Text = price3.ToString();
+            }
+            else
+            {
+                Text = "Unsufficient ballence";
+                linkLabel3.Text = "Unsufficient ballence";
+                await Task.Delay(500);
+                Text = price3.ToString();
+                linkLabel3.Text = price3.ToString();
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            while (isRunning)
+            {
+                money = money +(((multiplyer))*amountofclickers)/5;
+                lblmoney.Text = $"Money: ${money}";
+                await Task.Delay(Convert.ToInt32(clickspeed));
+                Text = multiplyer + "X";
+                linkLabel4.Text = multiplyer + "X";
+                Text = (clickspeed / 1000).ToString() + "s";
+                linkLabel5.Text = (clickspeed / 1000).ToString() + "s";
+                Text = amountofclickers.ToString();
+                linkLabel6.Text = amountofclickers.ToString();
+            }
+
+        }
+       
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Text = multiplyer+"X";
+            linkLabel4.Text = multiplyer+"X";
+        }
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Text = (clickspeed/1000).ToString()+"s";
+            linkLabel5.Text = (clickspeed / 1000).ToString() + "s";
+        }
+
+        private void linkLabel6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Text = amountofclickers.ToString();
+            linkLabel6.Text = amountofclickers.ToString();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Text = price2.ToString();
+            linkLabel2.Text = price2.ToString();
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (money >= price2)
+            {
+                money = money - price2;
+                clickspeed = clickspeed - (clickspeed * 0.1);
+                linkLabel2.Text = "Purchase complete";
+                await Task.Delay(500);
+                price2 = price2 * 3;
+                Text = price2.ToString();
+                linkLabel2.Text = price1.ToString();
+            }
+            else
+            {
+                Text = "Unsufficient ballence";
+                linkLabel2.Text = "Unsufficient ballence";
+                await Task.Delay(500);
+                Text = price2.ToString();
+                linkLabel2.Text = price1.ToString();
+            }
+            
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Text = price1.ToString();
+            linkLabel1.Text = price1.ToString();
+
+        }    
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            if (money >= price1)
+            {
+                money = money - price1;
+                lblmoney.Text = "Money: $" + money;
+                multiplyer = multiplyer + (price1 * 0.1);
+                linkLabel1.Text = "Purchase complete";
+                await Task.Delay(500);
+                price1 = price1 * 3;
+                Text = price1.ToString();
+                linkLabel1.Text = price1.ToString();
+            }
+            else
+            {
+                Text = "Unsufficient ballence";
+                linkLabel1.Text = "Unsufficient ballence";
+                await Task.Delay(500);
+                Text = price1.ToString();
+                linkLabel1.Text = price1.ToString();
+            }
+            
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Text = "Money: $ " + money;
+            lblmoney.Text = "Money: $" + money;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Cookie.Enabled=false;
+            money = money + (1 * multiplyer);
+            lblmoney.Text = "Money: $" + money;
+            await Task.Delay(Convert.ToInt32(clickspeed));
+            Cookie.Enabled = true;
+        }
+    }
+}
